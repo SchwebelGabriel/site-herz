b2fe7c35a8d4e28e631f12cf976a34b703c4d73f
Administrador <gabriel.schwebel@newport.com.br>
2024-05-18 01:07:31 -0300

    Adicionado Rotas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link link-dark fw-medium me-1 ms-1 rounded " aria-current="page"
-                                href="/"><span class="ms-2 ms-md-0">Home</span></a>
+                                href="{{route('pagina.inicial')}}"><span class="ms-2 ms-md-0">Home</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link link-dark fw-medium me-1 ms-1 rounded" href="/produtos"><span
@@ -82,9 +82,9 @@
 
     </header>
 
-    <div class="container">
+   
         @yield('content')
-    </div>
+
 
     <div class="container bg-warning py-1 mt-5"></div>
     <footer class="bg-black py-4">
@@ -158,6 +158,9 @@
                 </div>
             </div>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
